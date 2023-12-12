2e65de3e523779d1bed81fd07d14bbdab9587138
AndreTeixeira-21126 <a21126@alunos.ipca.pt>
2023-12-12 14:51:42 +0000

    chore: added swagger documentation to api gateway

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const LogMockAdapter = require("./adapters/MockLogAdapter");
 const makeApp = require("./app");
 const dotenv = require('dotenv')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'AllCarsIn1 API',
+            version: '1.0.0',
+            description: 'Stand aggregator API',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
 
 dotenv.config();
 
 const app = makeApp(new LogMockAdapter(), [
     {url: '/users', host: process.env.AUTHSERVICE_URI},
-    {url: '/stands', host: process.env.STANDSSERVICE_URI},
-    {url: '/vehicle', host: process.env.VEHICLESSERVICE_URI},
-    ]);
+]);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.listen(3000, async () => {
     const logger = app.get('logAdapter');
     await logger.execute({from: 'api-gateway', data: 'Api Gateway running on http://localhost:3000', date: new Date(),status: 'info'});
